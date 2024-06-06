77796d271b30846f08a2fbeb4b21c82b712f6c44
shahid <wrteamshahid03@gmail.com>
2024-06-06 17:55:08 +0530

    footer added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import CustomerReviews from "./component/sections/CustomerReviews";
 import About from "./component/About";
 import Home from "./component/Home";
+import Footer from "./component/Footer";
 
 
 function App() {
   return (
     <div className="App">
       <Home />
+      <Footer/>
     </div>
   );
 }
